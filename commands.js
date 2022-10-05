38b7ed7df9632fb2e342ac9e644df0085b04f209
Sylvie <imonlysylveon@gmail.com>
2022-10-04 19:08:33 -0500

    added error checking to the identify function and command checking

--- commands.js
+++ commands.js
@@ -80,19 +80,22 @@ let commands = {
 
     // sends the username and id of the target in an embed format
     "identifyCommand": async (message, args, users) => {
-        // assigns the user with the tagged ID to target
-        let target = await users.fetch(commandRecipient(message, args)).then((user) => { return user });
+        try {
+            let target = await users.fetch(commandRecipient(message, args)).then((user) => { return user });
 
-        // creates and posts embed
-        const identityEmbed = new EmbedBuilder()
-        .setColor(0xf2d2d6)
-        .setTitle('Omggg silly how do you not know this? :face_with_hand_over_mouth:')
-        .setThumbnail('https://cdn.statically.io/img/cuddlyoctopus.com/wp-content/uploads/2019/11/KI-032A-Astolfo.png?quality=100&f=auto')
-        .addFields(
-            { name: 'Name:', value: target.username + '#' + target.discriminator },
-            { name: 'ID:', value: target.id }
-        );
-        message.channel.send({ embeds: [identityEmbed] });
+            // creates and posts embed
+            const identityEmbed = new EmbedBuilder()
+            .setColor(0xf2d2d6)
+            .setTitle('Omggg silly how do you not know this? :face_with_hand_over_mouth:')
+            .setThumbnail('https://cdn.statically.io/img/cuddlyoctopus.com/wp-content/uploads/2019/11/KI-032A-Astolfo.png?quality=100&f=auto')
+            .addFields(
+                { name: 'Name:', value: target.username + '#' + target.discriminator },
+                { name: 'ID:', value: target.id }
+            );
+            message.channel.send({ embeds: [identityEmbed] });
+        } catch {
+            message.channel.send('Sorry, but i\'m not too sure who that is :sweat:');
+        }
     },
 
     // sends a list of commands and their descriptions in an embed format
