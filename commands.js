d4d7bdf182129fce518a88b32ac20d40b5e24503
Sylvie <imonlysylveon@gmail.com>
2022-10-09 20:13:06 -0500

    updated help command

--- commands.js
+++ commands.js
@@ -130,9 +130,15 @@ let commands = {
         .setDescription('All of lil sylvie\'s commands\nMake sure to include the \'~\' prefix')
         .setThumbnail('https://cdn.statically.io/img/cuddlyoctopus.com/wp-content/uploads/2019/11/KI-032A-Astolfo.png?quality=100&f=auto')
         .addFields(
+            { name: '~boo', value: 'Gives a list of commands' },
             { name: '~uwu', value: 'Responds with a line from the bad furry rap' },
+            { name: '~feed', value: 'Give lil sylvie a snacc' },
+            { name: '~pp', value: 'Generate a random size pp' },
+            { name: '~femball', value: 'Answers a yes or no question' },
+            { name: '~femboyme', value: 'Turns you into a femboy' },
             { name: '~opinion', value: 'Gives out an opinion on certain users' },
             { name: '~compliment', value: 'Gives out a random compliment' },
+            { name: '~identify', value: 'Gives the name and ID of the specified user' },
             { name: '~help', value: 'Gives a list of commands' },
         );
         message.channel.send({ embeds: [helpEmbed] });
